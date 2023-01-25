124266cda46b83b00874030c5e6417c45310d29c
Alisa Zhurova <zhurova2021@gmail.com>
2023-01-25 09:05:22 +0300

    modified index.js file

--- src/index.js
+++ src/index.js
@@ -38,7 +38,25 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let sliceResult = [];
+    let result = [];
+    
+    function sliceIntoArr(expr, count){
+        for (let i = 0; i < expr.length; i += count){
+            const items = expr.slice(i, i + count);
+            sliceResult.push(items);
+        }
+        return sliceResult;
+    }
+  
+    let arr = sliceIntoArr(expr, 10);
+
+    for(let i = 0; i < arr.length; i++){
+         arr[i] = arr[i].replace(/00/g, '').replace(/10/g, '.').replace(/11/g, '-');
+         result.push(arr[i]);
+    }
+    return result.map(i => MORSE_TABLE[i] || ' ').join('');
+
 }
 
 module.exports = {
